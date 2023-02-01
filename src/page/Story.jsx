2fe8660232488018819b847b4789934e465e51a8
oyinkansola <adebayoalameen88@gmail.com>
2023-02-01 20:58:39 +0100

    Style: Added quote to the testimonial section and modified the style of the components

--- src/page/Story.jsx
+++ src/page/Story.jsx
@@ -8,6 +8,8 @@ import island from "../assets/island.jpg"
 import third from "../assets/third.png"
 import fifth from "../assets/fifth.png"
 import sixth from "../assets/sixth.jpg"
+import Up from "../assets/1.svg"
+import Bottom from "../assets/2.svg"
 
 import "../styles/Homepage.scss"
 
@@ -21,7 +23,7 @@ function Story() {
           </div>
           <div className="right">
             <h1>Our Story</h1>
-            <p>“We’re here to help you travel the world, make money selling real estate and get your business at the top of the search engines.”</p>
+            <p>We’re here to help you travel the world, make money selling real estate and get your business at the top of the search engines.</p>
             <p>AIRWEBER is a one-stop travel agency, real estate agency, and digital marketing company. We help you to plan your trip, find the right hotel, or buy a property online. For your business, we help with all your marketing needs so you can focus on doing what you do best.</p>
             <p>A travel agency to tour the world, travel with speed and get unrestricted global access via dual citizenship</p>
             <p>Feel the exhilarating air of freedom, travel the world with great adventure, escape the bureauCRAZY, Combining modern technology, and old-fashioned service we ensure every trip is special</p>
@@ -30,7 +32,7 @@ function Story() {
           <section className='section'>
             <div className="left">
               <h1>Our Mission</h1>
-              <p>“We’re here to help you travel the world, own property and get your business at the top of the search engines.” </p>
+              <p>We’re here to help you travel the world, own property and get your business at the top of the search engines.</p>
               <p>Gain digital freedom with AirWEBER!</p>
               <img src={travel} alt="travel" />
             </div>
@@ -73,21 +75,21 @@ function Story() {
                 <h2>Client Testimonials</h2>
                 <div className="whole">
                     <div className="card">
-                        <p>“I came in expecting an Oligarch who'll try to shove down suggestions and upsells down my throat, but my reality was I found a team genuinely willing to help me, they gave me incredibly innovative ideas, methods and info, and prompt execution on my choices, I really have found a long term partner.”</p>
+                        <p><span><img src={Up} alt="quote-icon" /></span>I came in expecting an Oligarch who'll try to shove down suggestions and upsells down my throat, but my reality was I found a team genuinely willing to help me, they gave me incredibly innovative ideas, methods and info, and prompt execution on my choices, I really have found a long term partner.<span><img src={Bottom} alt="quote-icon" /></span></p>
                         <div className="hold">
                             <img src={third} alt="" />
                             <span>Adeola Babalols - Delly Team</span>
                         </div>
                     </div>
                     <div className="card">
-                        <p>“They've been so up to date with tech and solutions its crazy, they're my goto hub for innovative ideas before they reach the public. You've made my life so much easier, Thank you Airweber”</p>
+                        <p><span><img src={Up} alt="quote-icon" /></span>They've been so up to date with tech and solutions its crazy, they're my goto hub for innovative ideas before they reach the public. You've made my life so much easier, Thank you Airweber<span><img src={Bottom} alt="quote-icon" /></span></p>
                         <div className="hold">
                             <img src={fifth} alt="" />
                             <span>Smart Niza - Techy</span>
                         </div>
                     </div>
                     <div className="card">
-                        <p>“Dual citizenship, I'm blown away by how much access this has given me, the AirWEBER team made the whole process easy, and an investment rather than expense, I'm proud of them, huge asset.”</p>
+                        <p> <span><img src={Up} alt="quote-icon" /></span>Dual citizenship, I'm blown away by how much access this has given me, the AirWEBER team made the whole process easy, and an investment rather than expense, I'm proud of them, huge asset.<span><img src={Bottom} alt="quote-icon" /></span></p>
                         <div className="hold">
                             <img src={sixth} alt="" />
                             <span>Tico Lohan</span>
